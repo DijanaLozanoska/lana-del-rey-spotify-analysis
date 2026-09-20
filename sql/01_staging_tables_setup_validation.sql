f7aac0245b6cf6d5eafd10f567a244aa68908bcd
Di <75589556+DijanaLozanoska@users.noreply.github.com>
2026-09-20 14:29:17 +0200

    Delete sql/01_staging_tables_setup_validation.sql

--- sql/01_staging_tables_setup_validation.sql
+++
@@ -1,5 +0,0 @@
--- Confirm raw_json_load structure exists and is empty before loading
-SELECT COUNT(*) FROM raw_json_load;
-
--- Confirm spotify_json_staging structure is in place
-SELECT COUNT(*) FROM spotify_json_staging;
